d5a212c8282c14a3024aa157096590d174731057
Will Bush <williamdcbush@gmail.com>
2019-12-21 12:31:03 +0000

    layout added to Home.js

--- plan-coding-challenge/src/App.js
+++ plan-coding-challenge/src/App.js
@@ -7,7 +7,7 @@ import About from './Components/About'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Switch>
         <Route
           path='/' exact
--- plan-coding-challenge/src/Components/Home.js
+++ plan-coding-challenge/src/Components/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import './Home.css'
 
 class Home extends React.Component {
 
     render() {
 
         return (
-            <div>
-                <h1>HOME PAGE!</h1>
+            <div className="home">
+                <header>
+                    Plan.com MovieFinder
+                </header>
+                <aside>
+                    List
+                </aside>
+                <main>
+                    Main
+                </main>
+                <footer>
+                    Footer
+                </footer>
             </div>
         )
     }
